ea1966f802e46d59d8a914c8c49ad62075496bd8
Roopesh Rajashekharaiah Nataraja <roopeshr@codeaurora.org>
2018-01-23 15:31:48 -0800

    Add inaddr.h header file
    Change-Id: Iad92c39fb729538cf51bf9d9037b15515104b453

--- libc/include/bits/in_addr.h
+++ libc/include/bits/in_addr.h
@@ -36,8 +36,7 @@
 #include <sys/cdefs.h>
 #include <stdint.h>
 
-/** An integral type representing an IPv4 address. */
-typedef uint32_t in_addr_t;
+#include <inaddr.h>
 
 /** A structure representing an IPv4 address. */
 struct in_addr {
